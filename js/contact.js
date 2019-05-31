4da96ba381427f927c5bea486c06979ba790a7be
dukkona <dukkon.a@gmail.com>
2019-05-31 20:52:53 +0200

    init commit

---
+++ js/contact.js
@@ -0,0 +1,77 @@
+$(document).ready(function(){
+    
+    (function($) {
+        "use strict";
+
+    
+    jQuery.validator.addMethod('answercheck', function (value, element) {
+        return this.optional(element) || /^\bcat\b$/.test(value)
+    }, "type the correct answer -_-");
+
+    // validate contactForm form
+    $(function() {
+        $('#contactForm').validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 4
+                },
+                subject: {
+                    required: true,
+                    minlength: 4
+                },
+                email: {
+                    required: true,
+                    email: true
+                },
+                message: {
+                    required: true,
+                    minlength: 20
+                }
+            },
+            messages: {
+                name: {
+                    required: "A név mező kitöltése kötelező",
+                    minlength: "A név mezőnek legalább 4 karakterből kell állnia"
+                },
+                subject: {
+                    required: "A tárgy mező kitöltése kötelező",
+                    minlength: "A tárgy mezőnek legalább 4 karakterből kell állnia"
+                },
+                email: {
+                    required: "Az e-mail mező kitöltése kötelező"
+                },
+                message: {
+                    required: "Az e-mail szövege nem lehet üres",
+                    minlength: "Az üzenetnek legalább 20 karakterből kell állnia"
+                }
+            },
+            submitHandler: function(form) {
+                $(form).ajaxSubmit({
+                    type:"POST",
+                    data: $(form).serialize(),
+                    url:"contact_process.php",
+                    success: function() {
+                        $('#contactForm :input').attr('disabled', 'disabled');
+                        $('#contactForm').fadeTo( "slow", 1, function() {
+                            $(this).find(':input').attr('disabled', 'disabled');
+                            $(this).find('label').css('cursor','default');
+                            $('#success').fadeIn()
+                            $('.modal').modal('hide');
+		                	$('#success').modal('show');
+                        })
+                    },
+                    error: function() {
+                        $('#contactForm').fadeTo( "slow", 1, function() {
+                            $('#error').fadeIn()
+                            $('.modal').modal('hide');
+		                	$('#error').modal('show');
+                        })
+                    }
+                })
+            }
+        })
+    })
+        
+ })(jQuery)
+})
